7e7b9052b4dd4d056d94b13f08a0cb883f6807f4
xiaojin <j.xiao.edin@gmail.com>
2015-03-12 22:57:54 +1100

    POC initialization

---
+++ News/NewsItemCell.h
@@ -0,0 +1,13 @@
+//
+//  NewsItemCell.h
+//  News
+//
+//  Created by Martin on 3/12/15.
+//  Copyright (c) 2015 Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsItemCell : UITableViewCell
+
+@end
---
+++ News/NewsItemCell.m
@@ -0,0 +1,23 @@
+//
+//  NewsItemCell.m
+//  News
+//
+//  Created by Martin on 3/12/15.
+//  Copyright (c) 2015 Martin. All rights reserved.
+//
+
+#import "NewsItemCell.h"
+
+@implementation NewsItemCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
