e51b14cd71aeb3b7849061b89e90fa78ffa0d1ea
xiaojin <j.xiao.edin@gmail.com>
2015-03-13 11:46:11 +1100

    polish ViewController code

--- News/ViewController.m
+++ News/ViewController.m
@@ -31,22 +31,27 @@
     _tableView.frame =CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
     _tableView.delegate=self;
     _tableView.dataSource = self;
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)initFootView
+{
     CGFloat footbtnHeight = 35.0f;
     CGFloat footcontentPaddingLeft = 8.0f;
     CGFloat footcontentPaddingTop = 2.0f;
     FootView *footbutton = [[FootView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, footbtnHeight) withButtonFrame:CGRectMake(footcontentPaddingLeft,footcontentPaddingTop , (self.view.frame.size.width-2*footcontentPaddingLeft), (footbtnHeight-2*footcontentPaddingTop))];
     footbutton.delegate = self;
     _tableView.tableFooterView = footbutton;
-    self.index = 0+ SHOWITEMS;
-    _showFrameArray =[NSArray arrayWithArray:[_newsFrameArray subarrayWithRange:NSMakeRange(0, self.index)]];
-    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
-- (void)viewDidAppear:(BOOL)animated
+- (void)initDataSource
 {
-    [super viewDidAppear:animated];
-    [self.tableView reloadData];
+    self.index = 0+ SHOWITEMS;
+    _showFrameArray =[NSArray arrayWithArray:[_newsFrameArray subarrayWithRange:NSMakeRange(0, self.index)]];
 }
+
 #pragma mark - tableview data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
